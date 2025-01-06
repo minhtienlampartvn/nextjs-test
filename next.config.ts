b1916e939f59120c8a0723763151e65435d50a11
[Lampart] Vo Minh Tien <minh_tien@lampart-vn.com>
2025-01-06 15:47:19 +0700

    #update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
 };
 
-export default nextConfig;
+module.exports = nextConfig
+// export default nextConfig;
